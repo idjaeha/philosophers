02888b93b5a221a0c52124bacc402b87cd115351
idjaeha <jaehanim1@gmail.com>
2022-01-31 03:22:13 +0900

    불필요한 코드 삭제

--- philo/srcs/act.c
+++ philo/srcs/act.c
@@ -6,7 +6,7 @@
 /*   By: jayi <jayi@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 13:46:54 by jayi              #+#    #+#             */
-/*   Updated: 2022/01/31 02:44:50 by jayi             ###   ########.fr       */
+/*   Updated: 2022/01/31 03:15:13 by jayi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 static void	taken_fork(t_philo *philo)
 {
-	pthread_mutex_lock(philo->left);
 	pthread_mutex_lock(&philo->is_end);
+	pthread_mutex_unlock(&philo->is_end);
 	{
+		pthread_mutex_lock(philo->left);
 		print_message(get_mseconds(), MSG_FORK, philo->idx);
 	}
-	pthread_mutex_unlock(&philo->is_end);
-	pthread_mutex_lock(philo->right);
 	pthread_mutex_lock(&philo->is_end);
+	pthread_mutex_unlock(&philo->is_end);
 	{
+		pthread_mutex_lock(philo->right);
 		print_message(get_mseconds(), MSG_FORK, philo->idx);
 	}
-	pthread_mutex_unlock(&philo->is_end);
 }
 
 static void	eating(t_philo *philo)
@@ -49,25 +49,25 @@ static void	sleeping(t_philo *philo)
 {
 	time_t	now;
 
+	pthread_mutex_lock(&philo->is_end);
+	pthread_mutex_unlock(&philo->is_end);
 	pthread_mutex_unlock(philo->left);
 	pthread_mutex_unlock(philo->right);
-	pthread_mutex_lock(&philo->is_end);
 	{
 		now = get_mseconds();
 		philo->act_end = philo->var->time.sleep + now;
 		print_message(now, MSG_SLEEPING, philo->idx);
+		idle(get_mseconds(), philo->var->time.sleep);
 	}
-	pthread_mutex_unlock(&philo->is_end);
-	idle(get_mseconds(), philo->var->time.sleep);
 }
 
 static void	thinking(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->is_end);
+	pthread_mutex_unlock(&philo->is_end);
 	{
 		print_message(get_mseconds(), MSG_THINKING, philo->idx);
 	}
-	pthread_mutex_unlock(&philo->is_end);
 }
 
 void	*act(void *data)
